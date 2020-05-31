4385eea0de7dbe5980f8f19acfc8484c691e4e4f
Azad <azad00001947@gmail.com>
2020-05-31 12:31:52 +0530

    fixed CodeVerification

--- components/screens/CodeVerification.js
+++ components/screens/CodeVerification.js
@@ -8,121 +8,64 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import {authentication, twoFactor} from '../Images';
+import {styles} from '../../utils/globalStyleSheet';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
 
 export default function CodeVerification(props) {
   return (
     <View style={styles.container}>
-      <View style={{width: '100%', height: 300}}>
-        <Image
-          source={authentication}
-          style={styles.img}
-          resizeMode={'contain'}
-        />
+      <View style={{alignItems: 'center'}}>
+        <Image source={authentication} style={styles.img} />
+        <Text style={styles.tagLine}>
+          "just one step more. we are here to help you. stay home stay safe."
+        </Text>
       </View>
 
-      <Text style={styles.tagLine}>
-        "just one step more. we are here to help you. stay home stay safe."
-      </Text>
+      <View style={styles.inputView}>
+        <TextInput
+          keyboardType={'phone-pad'}
+          style={[styles.card, {color: '#fff'}]}
+          placeholder={'verification code'}
+          placeholderTextColor={'#6961ff'}
+          value={props.code}
+          onChangeText={(num) => props.setCode(num)}
+        />
+        {!props.isValidCode ? (
+          <Text style={styles.error}>{props.error}</Text>
+        ) : (
+          <Text style={styles.error}>{props.isValidCode}</Text>
+        )}
 
-      <View
-        style={{
-          width: '100%',
-          flex: 1,
-          alignItems: 'center',
-          // justifyContent: 'center',
-        }}>
-        <View>
-          <TextInput
-            autoFocus={true}
-            keyboardType={'phone-pad'}
-            style={[styles.card, {color: '#fff'}]}
-            placeholder={'verification code'}
-            value={props.code}
-            onChangeText={(num) => props.setCode(num)}
-          />
-          {!props.isValidCode ? (
-            <Text style={styles.error}>{props.error}</Text>
-          ) : (
-            <Text style={styles.error}>{props.isValidCode}</Text>
-          )}
-        </View>
         <TouchableOpacity
-          style={[styles.card, {backgroundColor: '#a19cf8'}]}
+          style={[styles.card, styles.button]}
           onPress={props.handleSubmit}>
-          <Text style={styles.button}>SUBMIT</Text>
+          <Text style={styles.submit}>SUBMIT</Text>
         </TouchableOpacity>
+      </View>
+
+      <View style={styles.covidView}>
         <View
-          style={{flexDirection: 'row', alignItems: 'center', marginTop: 5}}>
-          <Text style={{textAlign: 'center', fontSize: 12}}>
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginBottom: 10,}}>
+          <Text
+            style={{
+              fontSize: hp('2.5%') > wp('2.5%') ? hp('2.5%') : wp('2.5%'),
+            }}>
             din't get verification code??
           </Text>
           <TouchableOpacity onPress={props.sendCodeAgain}>
             <Text style={styles.link}> Send Again</Text>
           </TouchableOpacity>
         </View>
-      </View>
-
-      <View style={{width: '100%', backgroundColor: 'pink'}}>
         <Text style={styles.covid}>Covid-19: Stay Home, Stay Safe</Text>
       </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    // justifyContent: 'space-between',
-  },
-  img: {
-    width: '100%',
-    height: 350,
-  },
-  covid: {
-    height: 30,
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    fontWeight: 'bold',
-    color: '#6961ff',
-  },
-  card: {
-    width: 250,
-    height: 36,
-    borderRadius: 100 / 2,
-    backgroundColor: '#a19cf8',
-    padding: 10,
-    fontWeight: 'bold',
-    // margin: 10,
-  },
-  link: {
-    fontSize: 15,
-    color: 'blue',
-    fontWeight: 'bold',
-  },
-  button: {
-    textAlignVertical: 'center',
-    textAlign: 'center',
-    fontWeight: 'bold',
-    color: '#fff',
-  },
-
-  tagLine: {
-    width: '80%',
-    textAlign: 'center',
-    padding: 10,
-    fontSize: 15,
-    color: '#6961ff',
-    // marginTop: 5,
-    marginBottom: 10,
-    // fontFamily: 'sans-serif-light',
-    fontFamily: 'Merriweather-BlackItalic',
-    fontWeight: 'bold',
-    fontStyle: 'italic',
-  },
-  error: {
-    // color: '#6961ff',
-    color: 'red',
-    paddingLeft: 18,
-  },
-});
